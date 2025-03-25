b78be660a297ac9a5ec4c75b9ba8191892a388d6
Stephany <stephanymarchenna@gmail.com>
2025-03-25 12:16:18 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.version import PygameVersion
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close Window
+            quit()  # End pygame.
